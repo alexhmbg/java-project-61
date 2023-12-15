128b48685906e097b8920e0b0def8c4328fa2239
Alexande Hamburg <uplink205@gmail.com>
2023-12-15 16:55:49 +0200

    update GameEven.java

--- app/src/main/java/hexlet/code/games/GameEven.java
+++ app/src/main/java/hexlet/code/games/GameEven.java
@@ -7,19 +7,17 @@ import java.util.Scanner;
 public class GameEven {
 	public static void start() {
 		GameGreet.start();
-
 		System.out.println("Answer 'yes' if the number is even, otherwise answer 'no'.");
+
 		int questionIndex = 0;
 		while (questionIndex < 3) {
 			int randomNum = Engine.randomNumber();
-			String correctAnswer = randomNum % 2 == 0 ? "yes" : "no";
 
-			// >> to Engine correctAnswer + playerAnswer
 			System.out.println("Question: " + randomNum);
 			System.out.print("Your answer: ");
 			Scanner answerScanner = new Scanner(System.in);
 			String playerAnswer = answerScanner.nextLine();
-
+			String correctAnswer = randomNum % 2 == 0 ? "yes" : "no";
 			boolean bothCorrect = playerAnswer.equals(correctAnswer);
 
 			if (bothCorrect) {
