dd3f26bbbc549460d9b18ddbf63acb01dc776e0b
Alexande Hamburg <uplink205@gmail.com>
2023-12-16 13:41:12 +0200

    update GameCalc.java, update GameEven.java, update Greet.java

--- app/src/main/java/hexlet/code/games/GameEven.java
+++ app/src/main/java/hexlet/code/games/GameEven.java
@@ -6,32 +6,13 @@ import java.util.Scanner;
 
 public class GameEven {
 	public static void start() {
-		GameGreet.start();
-		System.out.println("Answer 'yes' if the number is even, otherwise answer 'no'.");
-
-		int questionIndex = 0;
-		while (questionIndex < 3) {
-			int randomNum = Engine.randomNumber();
-
-			System.out.println("Question: " + randomNum);
-			System.out.print("Your answer: ");
-			Scanner answerScanner = new Scanner(System.in);
-			String playerAnswer = answerScanner.nextLine();
-			String correctAnswer = randomNum % 2 == 0 ? "yes" : "no";
-			boolean bothCorrect = playerAnswer.equals(correctAnswer);
-
-			if (bothCorrect) {
-				System.out.println("Correct!");
-				if (questionIndex == 2) {
-					System.out.println("Congratulations, " + GameGreet.playerName + "!");
-				}
-				questionIndex++;
-			} else {
-				String wrongPlayerAnswer = "'" + playerAnswer + "' is wrong answer ;(. ";
-				String correctGameAnswer = "Correct answer was '" + correctAnswer + "'.";
-				System.out.println(wrongPlayerAnswer + correctGameAnswer);
-				break;
-			}
+		var description = "Answer 'yes' if the number is even, otherwise answer 'no'.";
+		int gameRounds = 3;
+		for(var currentRound = 0; currentRound < gameRounds; currentRound++) {
+			Engine.randomNumbers(1, 100, 1);
+			var question = Integer.toString(Engine.randomNumbers[0]);
+			String correctAnswer = Integer.parseInt(question) % 2 == 0 ? "yes" : "no";
+			Engine.gameRules(description, question, correctAnswer, currentRound, gameRounds);
 		}
 	}
 }
--- app/src/main/java/hexlet/code/games/Greet.java
+++ app/src/main/java/hexlet/code/games/Greet.java
@@ -2,7 +2,7 @@ package hexlet.code.games;
 
 import java.util.Scanner;
 
-public class GameGreet {
+public class Greet {
 	public static String playerName;
 	public static void start() {
 		System.out.println("\nWelcome to the Brain Games!");
