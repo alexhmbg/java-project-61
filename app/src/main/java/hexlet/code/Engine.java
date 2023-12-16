685b0626f385091fe14b170b7d9ad4e4d877317e
Alexande Hamburg <uplink205@gmail.com>
2023-12-16 13:40:23 +0200

    update Engine.java

--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -1,12 +1,65 @@
 package hexlet.code;
 
+import hexlet.code.games.Greet;
+
 import java.util.Scanner;
 
 public class Engine {
-	public static int randomNumber() {
-		// generate and return number
-		int Min = 1;
-		int Max = 100;
-		return Min + (int) (Math.random() * ((Max - Min) + 1));
+	public static int[] randomNumbers;
+	public static char randomOperator;
+	public static boolean gameOver = false;
+
+	public static void gameRules(String gameDescription, String question, String correctAnswer, int currentRound, int gameRounds) {
+		if (currentRound == 0) {
+			Greet.start();
+			System.out.println(gameDescription);
+		}
+		if (!gameOver) {
+			System.out.println("Question: " + question);
+			System.out.print("Your answer: ");
+			Scanner answerScanner = new Scanner(System.in);
+			String playerAnswer = answerScanner.nextLine();
+			isCorrect(correctAnswer, playerAnswer, currentRound, gameRounds);
+		}
+	}
+	public static int randomNumber(int min, int max) {
+		return min + (int) (Math.random() * ((max - min) + 1));
+	}
+
+	public static void randomNumbers(int min, int max, int amount) {
+		int[] randomNumbers = new int[amount];
+		for(var i = 0; i < amount; i++) {
+			randomNumbers[i] = randomNumber(min, max);
+		}
+		Engine.randomNumbers = randomNumbers;
+	}
+
+	public static void randomOperator() {
+		char[] operators = {'-', '+', '*'};
+		randomOperator = operators[randomNumber(0, 2)];
+	}
+
+	public static int randomOperation(int num1, int num2, char operator) {
+		return switch (operator) {
+			case '-' -> num1 - num2;
+			case '+' -> num1 + num2;
+			default -> num1 * num2;
+		};
+	}
+
+	public static void isCorrect(String correctAnswer, String playerAnswer, int currentRound, int gameRounds) {
+		var isCorrect = playerAnswer.equals(correctAnswer);
+		if (isCorrect && currentRound == gameRounds - 1) {
+			System.out.println("Correct!");
+			System.out.println("Congratulations, " + Greet.playerName + "!");
+		} else if (isCorrect) {
+			System.out.println("Correct!");
+		} else {
+			String wrongPlayerAnswer = "'" + playerAnswer + "' is wrong answer ;(. ";
+			String correctGameAnswer = "Correct answer was '" + correctAnswer + "'.";
+			System.out.println(wrongPlayerAnswer + correctGameAnswer);
+			System.out.println("Let's try again, " + Greet.playerName + "!");
+			gameOver = true;
+		}
 	}
 }
