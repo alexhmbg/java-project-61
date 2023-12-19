a45459f151d0209da59a957876babb3b2ff0ea60
Alexande Hamburg <uplink205@gmail.com>
2023-12-19 10:37:33 +0200

    update Utils.java

--- app/src/main/java/hexlet/code/Utils.java
+++ app/src/main/java/hexlet/code/Utils.java
@@ -5,7 +5,7 @@ public class Utils {
     private static final int MAX_DEFAULT_RANGE = 100;
 
     public static int randomNumber() {
-        return MIN_DEFAULT_RANGE + (int) (Math.random() * ((MIN_DEFAULT_RANGE - MAX_DEFAULT_RANGE) + 1));
+        return MIN_DEFAULT_RANGE + (int) (Math.random() * ((MAX_DEFAULT_RANGE - MIN_DEFAULT_RANGE) + 1));
     }
     public static int randomNumber(int min, int max) {
         return min + (int) (Math.random() * ((max - min) + 1));
