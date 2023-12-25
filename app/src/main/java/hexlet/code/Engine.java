9c493d0d2f5e50eee8effefe676f502414de0a3f
Alexande Hamburg <uplink205@gmail.com>
2023-12-25 07:35:05 +0200

    update Engine.java

--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -5,37 +5,6 @@ import java.util.Scanner;
 public class Engine {
     public static final int GAME_ROUNDS = 3;
 
-    public static void runGame(String[] questions, String[] answers, String description) {
-        System.out.println("\nWelcome to the Brain Games!");
-        System.out.print("May I have your name? ");
-        Scanner scanner = new Scanner(System.in);
-        var playerName = scanner.nextLine();
-        System.out.println("Hello, " + playerName + "!");
-        System.out.println(description);
-
-        for (var i = 0; i < GAME_ROUNDS; i++) {
-            System.out.println("Question: " + questions[i]);
-            System.out.print("Your answer: ");
-            Scanner answerScanner = new Scanner(System.in);
-            String playerAnswer = answerScanner.nextLine();
-            String correctAnswer = answers[i];
-            boolean isCorrect = playerAnswer.equals(answers[i]);
-
-            if (isCorrect) {
-                System.out.println("Correct!");
-            } else {
-                String wrongPlayerAnswer = "'" + playerAnswer + "' is wrong answer ;(. ";
-                String correctGameAnswer = "Correct answer was '" + correctAnswer + "'.";
-                System.out.println(wrongPlayerAnswer + correctGameAnswer);
-                System.out.println("Let's try again, " + playerName + "!");
-                return;
-            }
-
-        }
-
-        System.out.println("Congratulations, " + playerName + "!");
-    }
-
     public static void runGame(String[][] qa, String description) {
         System.out.println("\nWelcome to the Brain Games!");
         System.out.print("May I have your name? ");
